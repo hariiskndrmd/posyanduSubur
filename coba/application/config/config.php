92534943c3596ddfadb83a68813ea916af5db9c3
hariiskndrmd <hariiskandarmuda@gmail.com>
2024-06-28 13:58:35 +0700

    coba

--- coba/application/config/config.php
+++ coba/application/config/config.php
@@ -31,7 +31,7 @@ date_default_timezone_set('Asia/Jakarta');
 //  $config['base_url'] = $base_url; 
 // untuk xamppnya yang bermasalah, tidak muncul bootsrapnya
 
-$config['base_url'] = 'https://posyandusubur.com/';
+$config['base_url'] = 'https://posyandusubur.com/coba/';
 
 /*
 |--------------------------------------------------------------------------
